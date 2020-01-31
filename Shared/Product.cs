fa49d64c48207a13176d98d00522342f48cca7a7
Dima <dima.dimova@mail.bg>
2020-01-31 02:30:31 +0200

    added buttons, updated header and footer

---
+++ Shared/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace OnlineShop.Shared
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public string Image { get; set; }
+        public int Stars { get; set; }
+        public string Description { get; set; }
+        public string Info { get; set; }
+    }
+}
